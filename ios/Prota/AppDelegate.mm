31b4929c5c7193320d72fa568542296e5405b4ec
kyulee0103 <kyulee1338@gmail.com>
2023-05-04 02:46:58 +0900

    feat: android firebase 연동

--- ios/Prota/AppDelegate.mm
+++ ios/Prota/AppDelegate.mm
@@ -1,11 +1,14 @@
 #import "AppDelegate.h"
-
 #import <React/RCTBundleURLProvider.h>
+#import <Firebase.h>
+#import <FirebaseCore/FirebaseCore.h>
 
 @implementation AppDelegate
 
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   self.moduleName = @"Prota";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
